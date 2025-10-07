84b53beb50ecd9867f91589930b15789ff5059b1
Mekbib <mekbibtariku12@gmail.com>
2025-10-07 22:37:24 +0300

    Updated index.html with new changes

--- FidelShare/index.php
+++ FidelShare/index.php
@@ -7,32 +7,20 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>FidelShare</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <form action="" method="post">
+    <form action="login.php" method="post">
         <label for="email">Email</label>
-        <input type="email" placeholde="Email" id="email">
+        <input type="email" placeholder="Email" id="email" name="email" required>
+        
         <label for="password">Password</label>
-        <input type="password" placeholde="Password" id="password">
+        <input type="password" placeholder="Password" id="password" name="password" required>
+        
         <button type="submit">Login</button>  
+        
         <a href="register.php">Register</a>
         <a href="forgot_password.php">Forgot Password?</a>
-        <br>
-        <?php   
-            if(isset($_SESSION['error'])){
-                echo "<p style='color:red'>".$_SESSION['error']."</p>";
-                unset($_SESSION['error']);
-            }           
-            if(isset($_SESSION['success'])){
-                echo "<p style='color:green'>".$_SESSION['success']."</p>";
-                unset($_SESSION['success']);
-            }
-        ?>
-         
     </form>
-   
-    
 </body>
-
 </html>
-
