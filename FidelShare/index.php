ba550e030bdb6d426e01b44cd06e1cb1920e1155
Mekbib19 <mekbibtariku19@gmail.com>
2025-10-08 20:03:46 +0300

    Update index.php

--- FidelShare/index.php
+++ FidelShare/index.php
@@ -6,7 +6,7 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>FidelSharee</title>
+    <title>FidelShare</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
@@ -25,3 +25,4 @@ session_start();
 </body>
 
 </html>
+
