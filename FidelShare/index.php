8872fb7c79f3cd14a75239775a26d96d4308949f
Yibeltal-Birhanu <yibebra240@gmail.com>
2025-10-08 20:03:11 +0300

    Update index.php

--- FidelShare/index.php
+++ FidelShare/index.php
@@ -6,7 +6,7 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>FidelShare</title>
+    <title>FidelSharee</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
@@ -23,4 +23,5 @@ session_start();
         <a href="forgot_password.php">Forgot Password?</a>
     </form>
 </body>
+
 </html>
